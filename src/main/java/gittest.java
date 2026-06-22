5c51a13f1f4574d96403b9148958d4f8b42fb5a7
Jeongjinbeom <wlsqjal12345@naver.com>
2026-06-22 15:23:02 +0900

    test124

---
+++ src/main/java/gittest.java
@@ -0,0 +1,7 @@
+public class GitPushTest {
+
+    public static void main(String[] args) {
+        System.out.println("Git push test2026-06-22 successful.");
+	System.out.printlm("Git push can????1231415135325432624");
+    }
+}
