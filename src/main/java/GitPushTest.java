a076d1ee73c35e2c8e9428594275ae0552277b04
Jinbeom <91923857+Jeongjinbeom@users.noreply.github.com>
2026-06-22 17:59:32 +0900

    Update GitPushTest.java

--- src/main/java/GitPushTest.java
+++ src/main/java/GitPushTest.java
@@ -1,11 +1,36 @@
 public class GitPushTest {
 
-    public static void main(String[] args) {
-        System.out.println("Git push test2026-06-22 successful.");
+public static void main(String[] args) {
+	System.out.println("Git push test2026-06-22 successful.");
 	System.out.printlm("Git push can????1231415135325432624");
-		System.out.printlm("Git push can????1231415135325432624");
-		System.out.printlm("Git push can????1231415135325432624");
-		System.out.printlm("Git push can????1231415135325432624");
-		System.out.printlm("Git push can????1231415135325432624");
-    }
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624"); 
+	System.out.printlm("Git push can????1231415135325432624");
+			System.out.println("Git push test2026-06-22 successful.");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624"); 
+	System.out.printlm("Git push can????1231415135325432624");
+			System.out.println("Git push test2026-06-22 successful.");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624"); 
+	System.out.printlm("Git push can????1231415135325432624");
+			System.out.println("Git push test2026-06-22 successful.");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624"); 
+	System.out.printlm("Git push can????1231415135325432624");
+			System.out.println("Git push test2026-06-22 successful.");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624");
+	System.out.printlm("Git push can????1231415135325432624"); 
+	System.out.printlm("Git push can????1231415135325432624");
+	
+}
 }
